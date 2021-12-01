1f85add59de6667116f7599e62c4cb158c108129
hisamatsu yoshiyuki <hisasann25@gmail.com>
2021-12-01 22:26:06 +0900

    chore(eslint): update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,25 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended'
-  ],
-  plugins: [
-  ],
+  extends: ['react-app', 'plugin:import/typescript'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
   parserOptions: {
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
     ecmaFeatures: {
-      legacyDecorators: true
-    }
+      legacyDecorators: true,
+    },
   },
   // add your custom rules here
   rules: {
-    'dot-notation': 'off',
-    'no-console': 'off',
-    'prefer-const': 'off',
     '@typescript-eslint/no-object-literal-type-assertion': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
@@ -30,6 +28,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-empty-function': 'off',
-    'import/no-mutable-exports': 'off'
-  }
+    '@typescript-eslint/no-useless-constructor': 'off',
+    '@typescript-eslint/no-unused-expressions': 'off',
+  },
 }
